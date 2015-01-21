e022d42fb0f73187c36e351d2c1e1978fb400870
Mike Metral <metral@gmail.com>
2015-01-21 14:07:33 -0800

    Godep get only overlord_test

--- travis/build.sh
+++ travis/build.sh
@@ -5,6 +5,6 @@ go get github.com/tools/godep
 git clone https://github.com/metral/corekube $HOME/corekube
 
 git clone https://github.com/metral/corekube_travis
-pushd corekube_travis
+pushd corekube_travis/overlord_test
 godep get ./...
 popd
